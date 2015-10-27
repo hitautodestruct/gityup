265af07b36bbc9bcbd771ec1ed7570d714187ea6
hitautodestruct <yopraag@gmail.com>
2015-10-27 16:18:45 +0200

    Refactored and converted to nodejs module and added comments.

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 var exec = require('child_process').exec;
 
-var gity = (function () {
+module.exports = function () {
 
-  var repo = '';
+  var REPO = '';
 
+  // Setup repo name so we can access outside of the folder
   var setup = function ( repo_name ) {
-    repo = repo_name;
+    REPO = repo_name;
   };
 
+  // Returns the git path with the command
+  // Allows us to access the git repo outside of the given directory
+  var get_git_path = function () {
+    return 'git --git-dir='+ REPO +'/.git --work-tree='+ REPO;
+  };
+
+  // Outputs result to log
   var log_result = function(error, stdout, stderr) {
     console.log(stdout);
   };
 
+  // Excutes given command and outputs result
   var execute = function ( command ) {
       exec( command, log_result);
   };
 
+  // Clone the directory via http url
   var clone = function ( url ) {
     var command = 'git clone ' + url;
     execute( command );
   };
 
+  // Checkout branch
   var checkout = function ( branch_name ) {
-    var command = 'git --git-dir='+ repo +'/.git --work-tree='+ repo +' checkout ' + branch_name;
+    var git_path = get_git_path(),
+        command = git_path + ' checkout ' + branch_name;
+
     execute( command );
   };
 
+  // Show git status
   var status = function () {
-    var command = 'git --git-dir='+ repo +'/.git --work-tree='+ repo +' status';
+    var git_path = get_git_path(),
+        command = git_path + ' status';
+
     execute( command );
   };
 
@@ -38,4 +54,4 @@ var gity = (function () {
     status
   };
 
-})();
+};
